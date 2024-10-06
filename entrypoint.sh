cbe63bf165c1dc6526ccb8a83dfdf64c70c3bc9d
fisher772 <kovachvladislav99@gmail.com>
2024-10-06 23:39:16 +0500

    Spelling error

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@
 replace_aliases () {
   sed -i "s|LE_FQDN|${LE_FQDN}|g" /data/nginx/*.conf 2>/dev/null
   sed -i "s|LE_FQDN|${LE_FQDN}|g" /data/nginx/stream/*.conf 2>/dev/null
-  sed -i "s|value-default|${COUNTAINER_ALIAS}|g" /data/nginx/*.conf 2>/dev/null
-  sed -i "s|value-default|${COUNTAINER_ALIAS}|g" /data/nginx/stream/*.conf 2>/dev/null
+  sed -i "s|value-default|${CONTAINER_ALIAS}|g" /data/nginx/*.conf 2>/dev/null
+  sed -i "s|value-default|${CONTAINER_ALIAS}|g" /data/nginx/stream/*.conf 2>/dev/null
   sed -i "s|SERVER_ALIAS|${SERVER_ALIAS}|g" /data/nginx/stream/*.conf 2>/dev/null
 }
 
