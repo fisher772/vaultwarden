02034160c19d5889a27eb42fd6f781ac698858ae
fisher772 <kovachvladislav99@gmail.com>
2024-10-09 21:29:10 +0500

    Review and Patch Notes

--- Dockerfile
+++ Dockerfile
@@ -5,10 +5,10 @@ RUN chmod 755 /entrypoint.sh
 
 RUN mkdir -p /etc/rsyslog.d
 RUN mkdir -p /etc/logrotate.d
-RUN mkdir -p /data/nginx/stream
+RUN mkdir -p /data/nginx/stream conf ssl
 RUN mkdir -p /var/log/vaultwarden
 
-COPY settings/service-vaultwarden.conf /data/nginx/service-vaultwarden.conf
+COPY settings/service-vaultwarden.conf /data/nginx/conf/service-vaultwarden.conf
 COPY settings/stream/stream-vaultwarden.conf /data/nginx/stream/stream-vaultwarden.conf
 COPY settings/10-vaultwarden /etc/logrotate.d/10-vaultwarden
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 replace_aliases () {
-  sed -i "s|LE_FQDN|${LE_FQDN}|g" /data/nginx/*.conf 2>/dev/null
+  sed -i "s|LE_FQDN|${LE_FQDN}|g" /data/nginx/conf/*.conf 2>/dev/null
   sed -i "s|LE_FQDN|${LE_FQDN}|g" /data/nginx/stream/*.conf 2>/dev/null
-  sed -i "s|value-default|${CONTAINER_ALIAS}|g" /data/nginx/*.conf 2>/dev/null
+  sed -i "s|value-default|${CONTAINER_ALIAS}|g" /data/nginx/conf/*.conf 2>/dev/null
   sed -i "s|value-default|${CONTAINER_ALIAS}|g" /data/nginx/stream/*.conf 2>/dev/null
   sed -i "s|SERVER_ALIAS|${SERVER_ALIAS}|g" /data/nginx/stream/*.conf 2>/dev/null
 }
